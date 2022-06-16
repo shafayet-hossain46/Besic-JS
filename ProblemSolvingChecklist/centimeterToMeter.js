8c430d43ee1c4b0f03d1610a4799ad29d6acb924
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-06-16 19:40:37 +0600

    Added Centimeter To Meter

---
+++ ProblemSolvingChecklist/centimeterToMeter.js
@@ -0,0 +1,7 @@
+// Centimeter To Meters
+function centimeterToMeter(centimeter){
+    let meter = centimeter * 0.01;
+    return meter;
+}
+let totalMeter = centimeterToMeter(100);
+console.log(totalMeter);
